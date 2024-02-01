1caa879a4844338e779574bffc97f7e7103cf8a5
Asad ali <asad.ali@Inovabridage.com>
2024-02-02 00:49:14 +0500

    update

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,6 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.9.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.9.2/firebase-messaging.js');
-
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 firebase.initializeApp({
     apiKey: "AIzaSyBCTN6KOIpobVLPcxPCZeUd9rJ4t3uKYqo",
     authDomain: "student-management-4307e.firebaseapp.com",
@@ -8,8 +7,7 @@ firebase.initializeApp({
     storageBucket: "student-management-4307e.appspot.com",
     messagingSenderId: "838485293378",
     appId: "1:838485293378:web:797a98ec95c1b461e570db",
-    vapidKey:"BMgZuvjYNLOwmT7_PN0nDuTLM72X3a4ugWsV4mKoqfkRvzN4stGnL3MwpmTBqZgqoUtPe4EOMz5_2H5jJNMLyt0"
+    vapidKey: "BMgZuvjYNLOwmT7_PN0nDuTLM72X3a4ugWsV4mKoqfkRvzN4stGnL3MwpmTBqZgqoUtPe4EOMz5_2H5jJNMLyt0"
 
-})
-
+});
 const messaging = firebase.messaging();
