29cb6e81b20f77742705b8cd66cd14302508dd43
Hasnain Aslam <hasnainaslam808@gmail.com>
2024-02-12 23:33:07 +0500

    comm

--- src/app/shared/utility.service.spec.ts
+++ src/app/shared/utility.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { UtilityService } from './utility.service';
+// import { UtilityService } from './utility.service';
 
-describe('UtilityService', () => {
-  let service: UtilityService;
+// describe('UtilityService', () => {
+//   let service: UtilityService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UtilityService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(UtilityService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
